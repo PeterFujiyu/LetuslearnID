0d20cb79c0b8c573ccab8339418b88cfb83942da
Peter_369 | NotMyerrzz of Minecraft <88922024+PeterFujiyu@users.noreply.github.com>
2025-06-07 15:47:57 +0800

    feat: implement numeric email verification codes

--- server/email.js
+++ server/email.js
@@ -3,12 +3,14 @@ const config = require('./emailconfig.json');
 
 const transporter = nodemailer.createTransport(config.smtp);
 
-async function sendCode(to, code, subject = config.subject) {
+async function sendCode(to, code, ip, subject = config.subject) {
   const msg = {
     from: config.from,
     to,
     subject,
-    text: config.template.replace('{code}', code)
+    text: config.template
+      .replace('{code}', code)
+      .replace('{ip}', ip)
   };
   await transporter.sendMail(msg);
 }
