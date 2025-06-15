8a7c446d4d19a7f02b5ee358b0e3aeae7369fdfd
PeterFujiyu <jiyu.fu@outlook.com>
2025-06-15 16:54:11 +0800

    fix keys.js add { extractable: true }

--- server/lib/keys.js
+++ server/lib/keys.js
@@ -2,11 +2,11 @@ import { generateKeyPair, exportJWK } from 'jose';
 
 export async function createJWKSet() {
   if (process.env.JWT_PRIVATE) {
-    return { keys: [JSON.parse(process.env.JWT_PUBLIC)] };
+    return { keys: [JSON.parse(process.env.JWT_PRIVATE)] };
   }
-  const { privateKey } = await generateKeyPair('RS256');
-  const publicJwk = await exportJWK(privateKey);
-  publicJwk.use = 'sig';
-  publicJwk.alg = 'RS256';
-  return { keys: [publicJwk] };
+  const { privateKey } = await generateKeyPair('RS256', { extractable: true });
+  const privateJwk = await exportJWK(privateKey);
+  privateJwk.use = 'sig';
+  privateJwk.alg = 'RS256';
+  return { keys: [privateJwk] };
 }
